a8b376c9dcc79cccb6a04fefff24bb45ea7e7dc6
Steven Hu <husteven.91@gmail.com>
2016-10-05 13:17:43 +0200

    Add project structure

---
+++ src/main/java/com/academy/BrewxitApplication.java
@@ -0,0 +1,12 @@
+package com.academy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BrewxitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BrewxitApplication.class, args);
+	}
+}
