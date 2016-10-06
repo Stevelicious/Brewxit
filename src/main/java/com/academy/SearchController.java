6abb23129494adc73958ecc05b4a513a8fa93f6b
Math-ematics <junk@fernstedt.se>
2016-10-06 20:32:55 +0200

    Webbupdate

--- src/main/java/com/academy/SearchController.java
+++ src/main/java/com/academy/SearchController.java
@@ -3,14 +3,15 @@ package com.academy;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class SearchController {
 
-    @RequestMapping(value = "search", method = RequestMethod.GET)
-    public String search() {
-        ModelAndView search = new ModelAndView("index");
+    @RequestMapping(path="/", method = RequestMethod.GET)
+    public ModelAndView search() {
+        ModelAndView search = new ModelAndView("search");
 
-        return "/";
+        return search;
     }
 }
