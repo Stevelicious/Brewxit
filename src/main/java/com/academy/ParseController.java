2d215842ad0bbbdfb8b6a9585104a580b535d1bc
Lisa Solovyeva <vasilisa.solovyeva@gmail.com>
2016-10-07 13:38:47 +0200

    minor fix

--- src/main/java/com/academy/ParseController.java
+++ src/main/java/com/academy/ParseController.java
@@ -17,6 +17,8 @@ public class ParseController {
 
     private List<Butik> searchResult;
 
+    public reseplanerare_API reseplanerare_api = new reseplanerare_API();
+
     @GetMapping("/searchcoord")
     public ModelAndView search() {
         return new ModelAndView("searchcoord")
@@ -24,7 +26,8 @@ public class ParseController {
     }
 
     @PostMapping("/result")
-    public ModelAndView showResult(@RequestParam Reseplan reseplan) {
+    public ModelAndView showResult(@RequestParam String a, String b) {
+        Reseplan reseplan = reseplanerare_api.search(a, b);
         Parser parser = new Parser();
         searchResult = parser.getButiks(reseplan.destination.lon, reseplan.destination.lat);
 //        parser.printButiks();
